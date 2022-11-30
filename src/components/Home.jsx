ee0434ec9741e26eb9996f9a58c6ba143a845871
pawan <pawankhushrahoofficial@gmail.com>
2022-11-30 19:06:48 +0530

    home page designed and styled

---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='home'>
+        <main>
+            <h1>Solution</h1>
+            <p>Solution To All Your Problems</p>
+        </main>
+    </div>
+  )
+}
+
+export default Home
