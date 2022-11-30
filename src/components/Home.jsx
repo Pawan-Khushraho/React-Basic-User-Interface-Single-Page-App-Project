7add5b6ec7580ca0afca3915ed9484ac39a78d2c
pawan <pawankhushrahoofficial@gmail.com>
2022-11-30 19:57:11 +0530

    home page added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 
 const Home = () => {
   return (
+    <>
     <div className='home'>
         <main>
             <h1>Solution</h1>
             <p>Solution To All Your Problems</p>
         </main>
     </div>
+    </>
   )
 }
 
