5308b5af05d48d3a03f948859260611adff0f86e
pawan <pawankhushrahoofficial@gmail.com>
2022-12-02 19:34:45 +0530

    contact page created

--- src/App.js
+++ src/App.js
@@ -2,18 +2,21 @@ import{BrowserRouter as Router, Route, Routes} from "react-router-dom"
 import Header from "./components/Header";
 import Home from "./components/Home"
 import Footer from "./components/Footer";
+import Contact from "./components/Contact"
 
 import "./styles/app.scss"
 import "./styles/header.scss"
 import "./styles/home.scss"
 import "./styles/footer.scss"
+import "./styles/contact.scss"
 
 function App() {
   return (
     <Router>
       <Header/>
       <Routes>
-        <Route path="/" element={<Home/>}/> 
+        <Route path="/" element={<Home/>}/>
+        <Route path="/contact" element={<Contact/>}/> 
       </Routes>
       <Footer/>
     </Router>
